7390223334f569a3ba283c923d17131836b586c0
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2025-01-03 03:01:21 +0530

    First Day done

---
+++ first.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!") //used to print single statement
+console.table({name: "Kushal sharma", age :19, position : "GDG Organizer" }) //Used to Print multiple values in tabular form
+console.error("This is an error") //Used to print error message
+const AccountId = 9886    
+let Accountmail = "hello@gmail.com" //Always use let instead of var
+var accountpsswrd = "@3213" //issue of scope like this changes while changing other password
+AccountholderCity = "Noida"   //BAD PRACTICE
+console.table({AccountId,accountpsswrd,AccountholderCity})
