4bcbe5a836f4eedcf99ebff3c7a768746ea0b964
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2025-01-07 23:45:20 +0530

    comparison

--- third.js
+++ third.js
@@ -10,9 +10,24 @@ console.log(4 + 6 + "8"); */  //108
 // let goat = 100;
 // goat ++
 // console.log(goat);
-let x= 4
-let y = x++
-console.log(x,y) // O/p ==> 5 4 
-let x1= 4
-let y1 = ++x1
-console.log(x1,y1)  //O/p ==> 5 5
+// let x= 4
+// let y = x++
+// console.log(x,y) // O/p ==> 5 4 
+// let x1= 4
+// let y1 = ++x1
+// console.log(x1,y1)  //O/p ==> 5 5
+
+
+
+// some tricky comparisons
+console.log(null < 0) // converted to zero 
+console.log(null)
+
+console.log(null == 0) // conerted to NaN
+console.log(null)
+
+console.log(null >= 0) // conerted to NaN
+console.log(null)
+
+console.log(null <= 0)  // conerted to NaN
+console.log(null)
