1000e874e1f4ede0e76eae3b8b3f56ce8e6702eb
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2025-01-07 22:43:09 +0530

    str operations

--- third.js
+++ third.js
@@ -2,8 +2,17 @@
 let str1 = "Hello";
 let str2 = " World";
 // console.log(str1+str2);
-console.log("1"+4);                //14
+/*console.log("1"+4);                //14
 console.log("1"+ "87");             //187
 console.log("1"+4 + 6);          //146
-console.log(4 + 6 + "8");   //108
+console.log(4 + 6 + "8"); */  //108
 //<<<----Above code sis depend on toprimitive characterstics of the data type---->>>
+// let goat = 100;
+// goat ++
+// console.log(goat);
+let x= 4
+let y = x++
+console.log(x,y) // O/p ==> 5 4 
+let x1= 4
+let y1 = ++x1
+console.log(x1,y1)  //O/p ==> 5 5
