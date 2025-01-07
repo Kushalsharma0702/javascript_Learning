605eaf6f4dd5f508392adc55f4fb99f059b2b42b
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2025-01-07 19:57:15 +0530

    str operations

--- third.js
+++ third.js
@@ -2,3 +2,8 @@
 let str1 = "Hello";
 let str2 = " World";
 // console.log(str1+str2);
+console.log("1"+4);                //14
+console.log("1"+ "87");             //187
+console.log("1"+4 + 6);          //146
+console.log(4 + 6 + "8");   //108
+//<<<----Above code sis depend on toprimitive characterstics of the data type---->>>
